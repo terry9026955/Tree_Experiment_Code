e1cf464b0a1bdc0c328303249abe7d888e3d5019
terry <terry9026955@gmail.com>
2023-04-18 20:46:26 +0800

    面積分區模擬

--- SSTree_Ver3.py
+++ SSTree_Ver3.py
@@ -41,7 +41,7 @@ data_list = [(10, 12), (23, 3), (2, 28), (19, 23), (42, 8), (12, 47), (35, 34), 
 # data_list.sort()
 print(data_list)
 
-threshold = 4
+threshold = 5
 
 
 def get_centroid(data_list):
@@ -89,37 +89,56 @@ leaf = []
 centroid_list = []
 radius_list = []
 
+
+# 重覆面積公式
+def intersected_area(p1, p2, r1, r2):
+    d = math.sqrt((p1[0]-p2[0])*(p1[0]-p2[0]) + (p1[1]-p2[1])*(p1[1]-p2[1]))
+    if (d >= r1 + r2):
+        return
+    if (r1 > r2):
+        tmp = r1
+        r1 = r2
+        r2 = tmp
+    if (r2 - r1 >= d):
+        return math.pi * r1 * r1
+
+    ang1 = math.acos((r1 * r1 + d * d -
+                     r2 * r2) / (2 * r1 * d))
+    ang2 = math.acos((r2 * r2 + d * d -
+                     r1 * r1) / (2 * r2 * d))
+    return int((ang1 * r1 * r1) + (ang2 * r2 * r2) - (r1 * d * math.sin(ang1)))
+
+
 # 分區策略: 選擇兩子圓重疊的最小範圍來劃分
 # 1. 以最小重疊面積劃分 2. 以 兩圓半徑加總 - 兩圓中心直線距離之最小差來劃分
 
 # 類KD-tree的分區策略
 
 
-def region(df: list):
-    df1 = []
-    df2 = []
-    # temp = []
-    region_id = 0
-    lenth = len(df)  # 準備對半切
-    if lenth > threshold:
-        df1 = df[0:int(lenth/2)]    #
-        df2 = df[int(lenth/2):]
-        temp.append(df1)
-        temp.append(df2)
-        region(df1)
-        region(df2)
+# def region(df: list):
+#     df1 = []
+#     df2 = []
+#     # temp = []
+#     region_id = 0
+#     lenth = len(df)  # 準備對半切
+#     if lenth > threshold:
+#         df1 = df[0:int(lenth/2)]    #
+#         df2 = df[int(lenth/2):]
+#         temp.append(df1)
+#         temp.append(df2)
+#         region(df1)
+#         region(df2)
 
-        # print(df1, df2)  # 註解掉
-        # temp[region_id] = df1   # 註解掉
-        # region_id += 1
-        # temp[region_id] = df2   # 註解掉
-        # region_id += 1
-    else:
-        temp.remove(df)   # 註執行
-        leaf.append(df)   # 註執行
-        # region_id += 1
-        return
-    return region_id
+#         print("df1: ", df1, "df2: ", df2)  # 註解掉
+#         # temp[region_id] = df1   # 註解掉
+#         # region_id += 1
+#         # temp[region_id] = df2   # 註解掉
+#         # region_id += 1
+#     else:
+#         # temp.remove(df)   # 註執行
+#         leaf.append(df)   # 註執行
+#         # region_id += 1
+#         return
 
 # 使用分區策略的分割
 
@@ -128,7 +147,6 @@ def region2(df: list):
     df1 = []
     df2 = []
     # temp = []
-    region_id = 0
     split_index = findSplit(df)
     if len(df) > threshold:
         df1 = df[0:int(split_index)]    #
@@ -138,7 +156,8 @@ def region2(df: list):
         region2(df1)
         region2(df2)
 
-        # print(df1, df2)  # 註解掉
+        # print("df1: ", df1, "df2: ", df2)  # 註解掉
+
         # temp[region_id] = df1   # 註解掉
         # region_id += 1
         # temp[region_id] = df2   # 註解掉
@@ -148,14 +167,13 @@ def region2(df: list):
         leaf.append(df)   # 註執行
         # region_id += 1
         return
-    return region_id
 
 
 def findSplit(df: list):
     df1 = []
     df2 = []
     split_index = 2  # 預設小m
-    tempArea = None  # 紀錄最小面積
+    tempArea = 8787  # 紀錄最小面積
     splitArea = 8787    # 回傳最小面積
     former = int(len(df) * 0.4)
     lenth = int(len(df)) - 2
@@ -167,8 +185,16 @@ def findSplit(df: list):
         circle1_rad = get_radius(circle1_cen, df1)
         circle2_rad = get_radius(circle2_cen, df2)
 
-        tempArea = (circle1_rad + circle2_rad) - \
-            distance(circle1_cen, circle2_cen)
+        # 分區策略1:
+        tempArea = intersected_area(
+            circle1_cen, circle2_cen, circle1_rad, circle2_rad)
+        if (tempArea == None):
+            return i
+
+        # 分區策略2:
+        # tempArea = abs((circle1_rad + circle2_rad) -
+        #                distance(circle1_cen, circle2_cen))
+
         # 紀錄重疊最小的時候
         if tempArea < splitArea:
             splitArea = tempArea
@@ -207,15 +233,18 @@ def print_all_leaf():
 
 
 def main():
+
     centroid = get_centroid(data_list)
     redius = get_radius(centroid, data_list)
     print("centroid: ", centroid, "radius: ", redius)
 
     # print("not using split method")
-    id = region(data_list)
+
+    # region(data_list)
+
     # print(findSplit(data_list))   # 測試split的index
     # print("using split method")
-    # id2 = region2(data_list)
+    region2(data_list)
     # print("internal: ", temp)
 
     # for i in leaf:
@@ -226,6 +255,8 @@ def main():
     save_radius(leaf)
 
     print_all_leaf()
+
+    # print(intersected_area((0, 0), (0, 5), 3, 3))
 
 
 if __name__ == '__main__':
